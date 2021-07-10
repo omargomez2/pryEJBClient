7699b349e081544ea3d34fe546160c0844a2b21d
osgg <omargomez2@gmail.com>
2021-07-10 18:25:06 -0500

    v1

--- pryEJBClient/src/main/java/net/osgg/pryEJBClient/App.java
+++ pryEJBClient/src/main/java/net/osgg/pryEJBClient/App.java
@@ -13,7 +13,7 @@ public class App {
     public static void main( String[] args ) throws Exception {
     	ConverterBeanRemote converter = lookupHelloEJB();
 		System.out.println("Dame un saludo: " + converter.saludar());
-		System.out.println("Convierte 20 grados C a F: " + converter.convertToFahrenheit(20));
+		System.out.println("Convierte 20 grados C a F: " + converter.convertToFahrenheit(25));
     }
     
     
@@ -21,7 +21,8 @@ public class App {
 		final Hashtable<String, String> jndiProperties = new Hashtable<>();
 		jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
         //use HTTP upgrade, an initial upgrade requests is sent to upgrade to the remoting protocol
-        jndiProperties.put(Context.PROVIDER_URL,"remote+http://localhost:8080");
+        //jndiProperties.put(Context.PROVIDER_URL,"remote+http://localhost:8080");
+		jndiProperties.put(Context.PROVIDER_URL,"remote+http://192.168.0.104:8080");
         final Context context = new InitialContext(jndiProperties);
 
 		return (ConverterBeanRemote) context.lookup("ejb:/pryEJBConv/CoverterBean!net.osgg.ConverterBeanRemote");
