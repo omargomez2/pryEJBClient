31353bf16fbc7c26394f48704a81eaddeb089962
osgg <omargomez2@gmail.com>
2021-08-07 20:10:34 -0500

    Client code changed to work as localhost

--- pryEJBClient/src/main/java/net/osgg/App.java
+++ pryEJBClient/src/main/java/net/osgg/App.java
@@ -22,10 +22,10 @@ public class App {
 		final Hashtable<String, String> jndiProperties = new Hashtable<>();
 		jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
         //use HTTP upgrade, an initial upgrade requests is sent to upgrade to the remoting protocol
-        //jndiProperties.put(Context.PROVIDER_URL,"remote+http://localhost:8080");
-		jndiProperties.put(Context.PROVIDER_URL,"remote+http://192.168.0.104:8080");
+        jndiProperties.put(Context.PROVIDER_URL,"remote+http://localhost:8080");
+		//jndiProperties.put(Context.PROVIDER_URL,"remote+http://192.168.0.104:8080");
         final Context context = new InitialContext(jndiProperties);
 
-		return (ConverterBeanRemote) context.lookup("ejb:/pryEJBConv/CoverterBean!net.osgg.ConverterBeanRemote");
+		return (ConverterBeanRemote) context.lookup("ejb:/pryEJBConv/ConverterBean!net.osgg.ConverterBeanRemote");
 	}
 }
